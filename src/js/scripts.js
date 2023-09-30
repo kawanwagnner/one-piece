9c927fc19c6b317b7cc8b730cb9fe2b80abe2f4f
Kawan Wagnner <kawanwagnner7@gmail.com>
2023-09-29 23:30:54 -0300

    Finish 1.0 - Project with HTML, CSS and JS; PURE.

--- src/js/scripts.js
+++ src/js/scripts.js
@@ -1,68 +1,19 @@
-const personImg = document.querySelector(".person-img");
-const personId = document.querySelector(".person-id");
-const personName = document.querySelector(".person-name");
-const personAge = document.querySelector(".person-age");
-const personJob = document.querySelector(".person-job");
-const personBounty = document.querySelector(".person-bounty");
+document.addEventListener("DOMContentLoaded", function () {
+  let buttons = document.querySelectorAll(".button");
+  let personagens = document.querySelectorAll(".personagem");
 
-const buttonPrev = document.querySelector(".button-prev");
-const buttonNext = document.querySelector(".button-next");
+  buttons.forEach(function (button, index) {
+    button.addEventListener("click", function () {
+      personagens.forEach(function (personagem) {
+        personagem.classList.toggle(
+          "select",
+          personagem === personagens[index]
+        );
+      });
 
-let crewId = 1;
-let Person = 1;
-
-const fetchPersons = async () => {
-  const ApiOnePiece = await fetch(
-    `https://api.api-onepiece.com/characters/crew/1`
-  );
-
-  if (ApiOnePiece.status === 200) {
-    const data = await ApiOnePiece.json();
-    return data;
-  }
-};
-
-const renderPerson = async () => {
-  const data = await fetchPersons();
-
-  // let img = `./img/persons/${index}`;
-  let index = 0;
-
-  personImg.src = `src/img/persons/0.png`;
-  personId.innerHTML = `ID: ${data[index].id}`;
-  personName.innerHTML = `Name: ${data[index].french_name}`;
-  personAge.innerHTML = `Age: ${data[index].age}`;
-  personJob.innerHTML = `Job: ${data[index].job}`;
-  personBounty.innerHTML = `Bounty: ฿ ${data[index].bounty}`;
-
-  buttonNext.addEventListener("click", () => {
-    if (index < 10) {
-      index += 1;
-
-      buttonNext.style.display = "block";
-
-      personImg.src = `src/img/persons/${index}.png`;
-      personId.innerHTML = `ID: ${data[index].id}`;
-      personName.innerHTML = `Name: ${data[index].french_name}`;
-      personAge.innerHTML = `Age: ${data[index].age}`;
-      personJob.innerHTML = `Job: ${data[index].job}`;
-      personBounty.innerHTML = `Bounty: ฿ ${data[index].bounty}`;
-    }
+      buttons.forEach(function (btn) {
+        btn.classList.toggle("select", btn === button);
+      });
+    });
   });
-
-  buttonPrev.addEventListener("click", () => {
-    if (index > 0) {
-      index -= 1;
-
-      personImg.src = `src/img/persons/${index}.png`;
-      personId.innerHTML = `ID: ${data[index].id}`;
-      personName.innerHTML = `Name: ${data[index].french_name}`;
-      personAge.innerHTML = `Age: ${data[index].age}`;
-      personJob.innerHTML = `Job: ${data[index].job}`;
-      personBounty.innerHTML = `Bounty: ฿ ${data[index].bounty}`;
-      buttonPrev.style.display = "block";
-    }
-  });
-};
-
-renderPerson(Person);
+});
